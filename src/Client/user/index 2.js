6d3a6ef53de6898e9c7a3ef42a12f6fa469748ce
ZagrebaAlex <zagkrempaalex@gmail.com>
2025-05-22 18:17:05 +0300

    Added functions for the Location Class and created user/data endpoint

--- src/Client/user/index 2.js
+++ src/Client/user/index 2.js
@@ -1,5 +1,16 @@
-const load_profile_pic = (user_pic_link) => {
-    const img = document.createElement('img');
-        img.src = user_id;
-        return img;
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(";").shift();
 }
+
+// Check if auth cookie exists
+if (!getCookie("tagline_auth")) {
+  window.location.href = "/logIn";
+}
+
+const load_profile_pic = (user_pic_link) => {
+  const img = document.createElement("img");
+  img.src = user_id;
+  return img;
+};
--- src/Server/routing/User/userController.js
+++ src/Server/routing/User/userController.js
@@ -1,3 +1,29 @@
 /*
  * This Controller Conrols the API endpoint of User.
  */
+
+const express = require("express");
+const router = express.Router();
+const jwt = require("jsonwebtoken");
+
+const User = require("./User");
+const Location = require("../Location/Location");
+const Post = require("../Post/Post");
+const LogIn = require("../logIn/LogIn");
+
+router.get("/data", async (req, res) => {
+  const login = new LogIn();
+  const location = new Location();
+  const post = new Post();
+
+  const token = req.cookies.tagline_auth; // For knowing the logged in user
+
+  const userData = await login.getLoggedInUserData(token); // Get the logged in user data from the token
+  const locations = await location.getLocations();
+
+  const posts = await post.getPosts();
+
+  res.json({ ...userData, locations, posts });
+});
+
+module.exports = router;
