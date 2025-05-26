c0a177a47aef738bb188a8ca653e48e953ccd601
ZagrebaAlex <zagkrempaalex@gmail.com>
2025-05-26 21:52:14 +0300

    Added Leaderboard to friends page

--- src/Client/user/Friends/friends.js
+++ src/Client/user/Friends/friends.js
@@ -1,4 +1,4 @@
-const createFriendContainer = (username, src, friend_id) => {
+const createFriendContainer = (username, src, friend_id, points_collected) => {
   const friendContainer = document.createElement("div");
   friendContainer.className = "friend-container";
 
@@ -21,12 +21,64 @@ const createFriendContainer = (username, src, friend_id) => {
   return link;
 };
 
+const createLeaderboardContainer = (
+  username,
+  src,
+  friend_id,
+  points_collected,
+  index
+) => {
+  const friendContainer = document.createElement("div");
+  friendContainer.className = "friend-container-leaderboard";
+  const leftitems = document.createElement("div");
+  leftitems.className = "left-items";
+
+  const rank = document.createElement("span");
+  rank.textContent = `${index + 1}`;
+  rank.className = "friend-rank";
+
+  const img = document.createElement("img");
+  img.src = src || "/images/defaultProfile.png";
+  img.className = "profile-pic leaderboard-profile-pic";
+
+  const name = document.createElement("span");
+  name.textContent = username;
+  name.className = "friend-name";
+
+  const points = document.createElement("span");
+  points.textContent = `Points: ${points_collected}`;
+  points.className = "friend-points";
+
+  leftitems.appendChild(rank);
+  leftitems.appendChild(img);
+  leftitems.appendChild(name);
+  friendContainer.appendChild(leftitems);
+  friendContainer.appendChild(points);
+
+  const link = document.createElement("a");
+  link.href = `/user/profile?user_id=${friend_id}&from=friends`;
+  link.style.textDecoration = "none";
+  link.appendChild(friendContainer);
+
+  return link;
+};
+
 function add_element(parentId, container) {
   const parent = document.getElementById(parentId);
   if (parent && container) {
     parent.appendChild(container);
   }
 }
+
+fetch("/user/data", { credentials: "include" })
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    document.getElementById("points").textContent = data.points_collected;
+  })
+  .catch((error) => {
+    console.error("Error fetching user data:", error);
+  });
 
 fetch("/user/friends/get", { credentials: "include" })
   .then((response) => {
@@ -45,5 +97,23 @@ fetch("/user/friends/get", { credentials: "include" })
         friendUserID
       );
       add_element("friends-field", friendContainer);
+    });
+  });
+
+fetch("/user/friends/leaderboard", { credentials: "include" })
+  .then((response) => {
+    return response.json();
+  })
+  .then((leaderboard) => {
+    leaderboard.forEach((friend, index) => {
+      const { username, profileImage, UserID, points_collected } = friend;
+      const leaderboardContainer = createLeaderboardContainer(
+        username,
+        profileImage,
+        UserID,
+        points_collected,
+        index
+      );
+      add_element("leaderboard-field", leaderboardContainer);
     });
   });
